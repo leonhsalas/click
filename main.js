34a069a6aea3362ca1a2c45240434aad461c17ae
Leon Nicolhas <leonnhsalas@gmail.com>
2023-10-13 10:48:00 -0600

    but

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-const menu = document.getElementById('menu')
-const close = document.getElementById('close')
+const menu = document.getElementById('menu');
+const clos = document.getElementById('close');
+const nav = document.getElementById('nav');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('n')
+    clos.classList.toggle('n')
+    nav.classList.toggle('n')
+});
+clos.addEventListener('click', () => {
+    menu.classList.remove('n')
+    clos.classList.remove('n')
+    nav.classList.remove('n')
+});
